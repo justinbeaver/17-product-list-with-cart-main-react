407ca9b84cc29bb803df2a0b01dc07aa50d1d74e
justinbeaver <13700367+justinbeaver@users.noreply.github.com>
2024-12-17 13:05:52 +0200

    Add optimization

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo, useCallback } from "react";
 
 import { ProductsGrid } from "@/components/ProductsGrid/ProductsGrid";
 import { Cart } from "@/components/Cart/Cart";
@@ -7,7 +7,8 @@ import { ConfirmedModal } from "@/components/ConfirmedModal/ConfirmedModal";
 import s from "./Page.module.scss";
 
 export const Page = () => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState({});
+  const [productsOrdering, setProductsOrdering] = useState([]);
   const [cartProducts, setCartProducts] = useState({});
   const [isConfirmedModalShown, setIsConfirmedModalShown] = useState(false);
 
@@ -17,48 +18,71 @@ export const Page = () => {
       const url = "/data.json";
       const response = await fetch(url);
       const json = await response.json();
-      setProducts(json);
+
+      const productsMap = json.reduce((accumulator, product) => {
+        accumulator[product.name] = product;
+        return accumulator;
+      }, {});
+      setProducts(productsMap);
+
+      const productsOrderingMap = json.map((product) => {
+        return product.name;
+      });
+      setProductsOrdering(productsOrderingMap);
     };
     loadData();
   }, []);
 
-  const productsToGrid = products.map((product) => ({
-    imgMobile: product.image.mobile,
-    imgDesktop: product.image.desktop,
-    title: product.name,
-    category: product.category,
-    price: product.price,
-    count: cartProducts[product.name] || 0,
-  }));
+  const productsToGrid = useMemo(
+    () =>
+      productsOrdering.map((productName) => {
+        const product = products[productName];
 
-  const productsToCart = Object.keys(cartProducts).map((product) => {
-    for (const item of products) {
-      if (item.name === product) {
         return {
-          name: product,
-          price: item.price,
-          count: cartProducts[product],
+          imgMobile: product.image.mobile,
+          imgDesktop: product.image.desktop,
+          title: productName,
+          category: product.category,
+          price: product.price,
+          count: cartProducts[product.name] || 0,
         };
-      }
-    }
-  });
+      }),
+    [products, cartProducts, productsOrdering]
+  );
 
-  const productsToConfirmedModal = productsToCart.map((product) => {
-    for (const item of products) {
-      if (item.name === product.name) {
-        return { ...product, imgThumbnail: item.image.thumbnail };
-      }
-    }
-  });
+  const productsToCart = useMemo(
+    () =>
+      Object.keys(cartProducts).map((productName) => ({
+        name: productName,
+        price: products[productName].price,
+        count: cartProducts[productName],
+      })),
+    [products, cartProducts]
+  );
 
-  const handleAddToCart = (product) => {
+  const productsToConfirmedModal = useMemo(
+    () =>
+      Object.keys(cartProducts).map((productName) => {
+        const product = products[productName];
+
+        return {
+          imgThumbnail: product.image.thumbnail,
+          name: productName,
+          price: product.price,
+          count: cartProducts[productName],
+        };
+      }),
+    [products, cartProducts]
+  );
+
+  const handleAddToCart = useCallback((product) => {
     setCartProducts((prev) => ({
       ...prev,
       [product]: prev[product] ? prev[product] + 1 : 1,
     }));
-  };
+  }, []);
 
-  const handleSubtractFromCart = (product) => {
+  const handleSubtractFromCart = useCallback((product) => {
     setCartProducts((prev) => {
       const cart = { ...prev };
       if (cart[product] > 1) {
@@ -68,24 +92,24 @@ export const Page = () => {
       }
       return cart;
     });
-  };
+  }, []);
 
-  const handleRemoveFromCart = (product) => {
+  const handleRemoveFromCart = useCallback((product) => {
     setCartProducts((prev) => {
       const cart = { ...prev };
       delete cart[product];
       return cart;
     });
-  };
+  }, []);
 
-  const handleConfirmedModalToggle = () => {
+  const handleConfirmedModalToggle = useCallback(() => {
     setIsConfirmedModalShown((prev) => {
       if (prev) {
         setCartProducts({});
       }
       return !prev;
     });
-  };
+  }, []);
 
   return (
     <div className={s.page}>
